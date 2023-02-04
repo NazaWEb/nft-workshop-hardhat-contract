0173c2755f98cbbcbe7812c6e7dc1e60c2210043
Daniel Jimenez <daniel@djimenezdev.com>
2023-02-04 06:35:27 -0800

    config updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,6 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: {
     version: "0.8.4",
@@ -29,11 +13,11 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: "",
-      accounts: [""]
-    }
+      url: process.env.GOERLI_RPC || "",
+      accounts: [process.env.PRIVATE_ONE || ""],
+    },
   },
   etherscan: {
-    apiKey: "AEDRXVRA4TDK4SE1BTVZI3TV189HSG7VFN"
-  }
+    apiKey: "AEDRXVRA4TDK4SE1BTVZI3TV189HSG7VFN",
+  },
 };
